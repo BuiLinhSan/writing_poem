0afba851ef561575151daad2c2679b6d51c39455
Mossyfighting <builetrongphuoc@gmail.com>
2023-07-29 16:35:24 +0700

    feat: write poem scripts

---
+++ write_poem.py
@@ -0,0 +1,11 @@
+def write_poem(sentence1: list)-> str:
+    poem = '\n'.join(sentence1)
+    return poem
+
+if __name__ == '__main__':
+    sentence1 =\
+        [
+            '11111', 
+            '22222'
+        ]
+    print(write_poem(sentence1))
